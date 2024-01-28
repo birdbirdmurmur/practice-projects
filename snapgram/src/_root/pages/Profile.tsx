678183542b6555906cb2506f8e594afa17e19e5b
BirdBird <birdbirdmurmur@gmail.com>
2024-01-28 15:19:10 +0800

    Implement profile page & fix profile path

--- snapgram/src/_root/pages/Profile.tsx
+++ snapgram/src/_root/pages/Profile.tsx
@@ -1,13 +1,30 @@
-import { Link, useParams } from "react-router-dom"
+import { Link, Outlet, Route, Routes, useLocation, useParams } from "react-router-dom"
 
 import { useUserContext } from "@/context/AuthContext"
 import { useGetUserById } from "@/lib/react-query/queriesAndMutations"
+
 import { Loader } from "@/components/shared/Loader"
 import { Button } from "@/components/ui/button"
+import GridPostList from "@/components/shared/GridPostList"
+import { LikedPosts } from "@/_root/pages";
+
+type StatBlockProps = {
+    value: string | number
+    label: string
+}
+
+const StatBlock = ({ value, label }: StatBlockProps) => (
+    <div className="flex items-center gap-1.5">
+        <p className="small-semibold lg:body-bold text-primary-500">{value}</p>
+        <p className="small-medium lg:base-medium text-light-2">{label}</p>
+    </div>
+)
+
 
 const Profile = () => {
     const { id } = useParams()
     const { user } = useUserContext()
+    const { pathname } = useLocation()
 
     const { data: currentUser } = useGetUserById(id || "")
 
@@ -39,7 +56,9 @@ const Profile = () => {
                             </p>
                         </div>
                         <div className="flex gap-8 mt-10 items-center justify-center xl:justify-start flex-wrap z-20">
-                            statBlock
+                            <StatBlock value={currentUser.posts.length} label="Posts" />
+                            <StatBlock value={20} label="Followers" />
+                            <StatBlock value={10} label="Following" />
                         </div>
 
                         <p className="small-medium md:base-medium text-center xl:text-left mt-7 max-w-screen-sm">
@@ -64,7 +83,7 @@ const Profile = () => {
                         </div>
 
                         <div
-                        // className={`${user.id === id && "hidden"}`}
+                            className={`${user.id === id && "hidden"}`}
                         >
                             <Button
                                 type="button"
@@ -77,33 +96,45 @@ const Profile = () => {
                 </div>
             </div>
 
-            <div className="flex max-w-5xl w-full">
-                <Link
-                    to={`/profile/${id}`}
-                    className="profile-tab rounded-l-lg"
-                >
-                    <img
-                        src="/assets/icons/posts.svg"
-                        alt="posts"
-                        width={20}
-                        height={20}
-                    />
-                    Posts
-                </Link>
-                <Link
-                    to={`/profile/${id}/liked-posts`}
-                    className="profile-tab rounded-r-lg"
-                >
-                    <img
-                        src="/assets/icons/like.svg"
-                        alt="like"
-                        width={20}
-                        height={20}
-                    />
-                    Liked Posts
-                </Link>
-            </div>
+            {currentUser.$id === user.id && (
+                <div className="flex max-w-5xl w-full">
+                    <Link
+                        to={`/profile/${id}`}
+                        className={`profile-tab rounded-l-lg ${pathname === `/profile/${id}` && "!bg-dark-3"
+                            }`}>
+                        <img
+                            src="/assets/icons/posts.svg"
+                            alt="posts"
+                            width={20}
+                            height={20}
+                        />
+                        Posts
+                    </Link>
+                    <Link
+                        to={`/profile/${id}/liked-posts`}
+                        className={`profile-tab rounded-r-lg ${pathname === `/profile/${id}/liked-posts` && "!bg-dark-3"
+                            }`}>
+                        <img
+                            src="/assets/icons/like.svg"
+                            alt="like"
+                            width={20}
+                            height={20}
+                        />
+                        Liked Posts
+                    </Link>
+                </div>
+            )}
 
+            <Routes>
+                <Route
+                    index
+                    element={<GridPostList posts={currentUser.posts} showUser={false} />}
+                />
+                {currentUser.$id === user.id && (
+                    <Route path="/liked-posts" element={<LikedPosts />} />
+                )}
+            </Routes>
+            <Outlet />
         </div>
     )
 }
